4d5fdcf4c5269eaa133e81e2b240c673e77040c9
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2024-06-21 13:30:25 -0300

    adjust camera transmitter

--- camera_transmitter/scripts/camera_transmitter_node.py
+++ camera_transmitter/scripts/camera_transmitter_node.py
@@ -1,34 +1,21 @@
 #!/usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import cv2
 
 def transmit_camera_hdmi(width: int, height: int, desired_fps: float):
     cap = cv2.VideoCapture(0)
     
     # Set the desired frame rate
-    if desired_fps == 0:
-        desired_fps = cap.get(cv2.CAP_PROP_FPS)
-    original_fps = cap.get(cv2.CAP_PROP_FPS)
+    original_fps = 30
     frames_to_skip = int(original_fps/desired_fps) - 1
-    cap.set(cv2.CAP_PROP_FPS, desired_fps)
-    if original_fps == 0:
-        frames_to_skip = 5
-        print("Warning: Frame rate not available.")
-    if desired_fps == cap.get(cv2.CAP_PROP_FPS):
-        frames_to_skip = 0
-        print("Frame rate set successfully.")
-    print("Frame rate:", original_fps)
+
     print("Desired frame rate:", desired_fps)
     print("Frames to skip:", frames_to_skip)
-    frames_to_skip = 5
 
     # Create a custom window
     window_name = 'Robot Camera'
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     frame_count = 0
     while not rospy.is_shutdown():
@@ -64,4 +51,3 @@ if __name__ == '__main__':
         
     except rospy.ROSInterruptException:
         print("ROS node terminated.")
-    
