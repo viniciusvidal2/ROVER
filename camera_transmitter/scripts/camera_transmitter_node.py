10ed5a67b7cbe34af3f2e0e8527b3236ba967723
viniciusvidal2 <vinicius.vidal@engenharia.ufjf.br>
2024-06-24 09:51:13 -0300

    tested and fixed camera publishing to have FullHD resolution and lower fps

--- camera_transmitter/scripts/camera_transmitter_node.py
+++ camera_transmitter/scripts/camera_transmitter_node.py
@@ -14,8 +14,9 @@ def transmit_camera_hdmi(width: int, height: int, desired_fps: float):
     print("Frames to skip:", frames_to_skip)
 
     # Create a custom window
-    window_name = 'Robot Camera'
+    window_name = 'Vehicle Camera'
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     frame_count = 0
     while not rospy.is_shutdown():
@@ -23,15 +24,17 @@ def transmit_camera_hdmi(width: int, height: int, desired_fps: float):
         frame_count += 1
         if not ret:
             continue
-
-        # Resize the image to the desired size
-        cv2.resize(frame, (width, height))
         
         # Control the frame rate
         if frame_count == frames_to_skip + 1:
             frame_count = 0
+            
+            # Resize the image to the desired size
+            frame = cv2.resize(frame, (width, height))
+            
             # Display frame
-            cv2.imshow(window_name, frame)
+            frame = cv2.imshow(window_name, frame)
+            
             # Check for quit event
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -45,7 +48,7 @@ if __name__ == '__main__':
     
         width = rospy.get_param('~width', 1920)
         height = rospy.get_param('~height', 1080)
-        fps = rospy.get_param('~fps', 10)
+        fps = rospy.get_param('~fps', 5)
         
         transmit_camera_hdmi(width, height, fps)
         
