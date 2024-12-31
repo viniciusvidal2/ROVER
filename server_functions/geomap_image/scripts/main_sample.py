dcb77a104a1c5ff69755d3c03e074e56bef08b2a
viniciusvidal2 <viniciusvidal2@gmail.com>
2024-12-31 13:48:48 -0300

    code refactoring

--- server_functions/geomap_image/scripts/main_sample.py
+++ server_functions/geomap_image/scripts/main_sample.py
@@ -11,11 +11,14 @@ if __name__ == "__main__":
     rover_code = 0
     map_folder = folder
     desired_map_size = [1080, 1920]  # [lines, collumns]
-    robot_map_manager = MapManager(rover_code=rover_code, folder=map_folder, imsize=desired_map_size)
+    robot_map_manager = MapManager(
+        rover_code=rover_code, folder=map_folder, imsize=desired_map_size)
     # Call the point cloud and bev generations
     map_ptc = robot_map_manager.generateMapPtc()
-    map_bev, map_coords_json = robot_map_manager.generateMapBev(ptc_map_frame=map_ptc)
+    map_bev, map_coords_json = robot_map_manager.generateMapBev(
+        ptc_map_frame=map_ptc)
     # Save the files
-    robot_map_manager.saveMap(map_bev=map_bev, map_ptc=map_ptc, map_coords=map_coords_json)
+    robot_map_manager.saveMap(
+        map_bev=map_bev, map_ptc=map_ptc, map_coords=map_coords_json)
     end = time()
     print(f"Time taken: {end - start} s")
