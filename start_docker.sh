26bcebd767a79ead426065a66aad87a66ef7319d
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2025-01-02 12:10:18 -0300

    filling topics to record

--- start_docker.sh
+++ start_docker.sh
@@ -9,7 +9,7 @@ echo "ROS environment is running"
 # Directory to save the bag files
 SAVE_DIR="/home/rover/bags_debug"
 # Topics to record
-TOPICS="/livox/scan /mavros/imu/data /mavros/setpoint_raw/target_global /mavros/state /mavros/global_position/global /mavros/global_position/compass_hdg /mavros/mission/waypoints /mavros/home_position/home"
+TOPICS="/livox/scan /livox/imu /mavros/imu/data /mavros/setpoint_raw/target_global /mavros/state /mavros/global_position/global /mavros/global_position/compass_hdg /mavros/mission/waypoints /mavros/home_position/home"
 # Duration to record each bag file (in seconds)
 DURATION=30
 # Construct the filename
