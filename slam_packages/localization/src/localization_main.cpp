a6019d88097fdaf2e0ae6a4a341d8bdbf024f56d
viniciusvidal2 <viniciusvidal2@gmail.com>
2025-01-07 11:54:31 -0300

    localization node only runs if map exists

--- slam_packages/localization/src/localization_main.cpp
+++ slam_packages/localization/src/localization_main.cpp
@@ -6,15 +6,33 @@
 #include <memory>
 #include <functional>
 #include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include <ros/ros.h>
 
 #include "localization/localization_node.h"
 
+inline static bool directoryExists(const std::string &path) 
+{
+    struct stat info;
+    return (stat(path.c_str(), &info) == 0 && (info.st_mode & S_IFDIR));
+}
+
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "localization_node");
     ros::NodeHandle nh;
+    // If map folder does not exist, do not run localization
+    std::string map_folder = "", map_name = "";
+    nh.param("/map_data/home_relative_path", map_folder, static_cast<std::string>("Desktop/map_data"));
+    nh.param("/map_data/map_name", map_name, static_cast<std::string>("map"));
+    std::string map_path = std::string(std::getenv("HOME")) + "/" + map_folder + "/" + map_name;
+    if (!directoryExists(map_path))
+    {
+        ROS_WARN("Map with name %s does not exist yet, ending localization node ...", map_path.c_str());
+        return 0;
+    }
     LocalizationNode localization_node(nh);
     ros::spin();
     ros::shutdown();
