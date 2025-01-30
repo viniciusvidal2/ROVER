4adebecc25df2ade1c0935d15ff40fe0d9a0a53b
viniciusvidal2 <viniciusvidal2@gmail.com>
2025-01-30 16:58:38 -0300

    code refactoring. adding debug option to avoid prints

--- temperature_sensors/temperature_sensors.py
+++ temperature_sensors/temperature_sensors.py
@@ -6,56 +6,81 @@ import adafruit_bmp280
 import yaml
 import requests
 
-endpoint = "http://127.0.0.1:5000/temperatures/post"
 
-dht22_dict = {}
+def main(debug: bool = False):
+    """Reads the temperature and humidity from the sensors and sends the data to the rest api.
 
-spi = board.SPI()
-bmp280_dict = {}
+    Args:
+        debug (bool, optional): Prints debug option. Defaults to False.
+    """
+    # Rest API endpoint to send the temperature data
+    endpoint = "http://127.0.0.1:5000/temperatures/post"
 
-with open('sensors.yaml', 'r') as file:
-    sensors_list = yaml.safe_load(file)
-    
-    for sensor in sensors_list['bmp280']:
+    # Sensors data
+    dht22_dict = {}
+    bmp280_dict = {}
+    spi = board.SPI()
+
+    # Opening the sensors from the list in the yaml file
+    # Sensors setup process
+    with open('sensors.yaml', 'r') as file:
+        sensors_list = yaml.safe_load(file)
+
+        for sensor in sensors_list['bmp280']:
+            try:
+                bmp280_cs = DigitalInOut(getattr(board, sensor['cs_pin']))
+                bmp280_cs.direction = Direction.OUTPUT
+                bmp280_cs.value = 1
+                bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp280_cs)
+                bmp280_dict[sensor['name']] = bmp280
+            except:
+                if debug:
+                    print(f"Not found {sensor['name']}")
+
+        for sensor in sensors_list['dht22']:
+            try:
+                dht22_dict[sensor['name']] = adafruit_dht.DHT22(
+                    getattr(board, sensor['pin']))
+            except:
+                if debug:
+                    print(f"Not found {sensor['name']}")
+
+    # Main loop
+    while True:
+        sensors_data = {}
+        # Reading dht sensors
+        for name, dht22 in dht22_dict.items():
+            try:
+                temperature = dht22.temperature
+                humidity = dht22.humidity
+                if debug:
+                    print(
+                        f"{name} - Temperature: {temperature:.2f} - Humididty {humidity:.2f} %")
+                sensors_data[name] = {
+                    "temperature": temperature, "humidity": humidity}
+            except:
+                if debug:
+                    print(f"Not found DHT22 {name}")
+        # Reading bmp sensors
+        for name, bmp280 in bmp280_dict.items():
+            try:
+                temperature = bmp280.temperature
+                if debug:
+                    print(f"{name} - Temperature: {temperature:.2f} C")
+                sensors_data[name] = {"temperature": temperature}
+            except:
+                if debug:
+                    print(f"Not found BMP280 {name}")
+        # Sending POST request to the rest api
         try:
-            bmp280_cs = DigitalInOut(getattr(board, sensor['cs_pin']))
-            bmp280_cs.direction = Direction.OUTPUT
-            bmp280_cs.value = 1
-            bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp280_cs)
-            bmp280_dict[sensor['name']] = bmp280
-        except:
-            print(f"Not found {sensor['name']}")
-            
-    for sensor in sensors_list['dht22']:
-        try:
-            dht22_dict[sensor['name']] = adafruit_dht.DHT22(getattr(board, sensor['pin']))
-        except:
-            print(f"Not found {sensor['name']}")
+            if sensors_data:
+                _ = requests.post(endpoint, json=sensors_data)
+        except Exception as e:
+            if debug:
+                print(f"Error while making request: {e}")
+        # We must wait 2 seconds before reading the sensors again
+        time.sleep(2)
 
-while True:
-    sensors_data = {}
-    
-    for name, dht22 in dht22_dict.items():
-        try:
-            temperature = dht22.temperature
-            humidity = dht22.humidity
-            print(f"{name} - Temperature: {temperature:.2f} - Humididty {humidity:.2f} %")
-            sensors_data[name] = {"temperature": temperature, "humidity": humidity}
-        except:
-            print(f"Not found DHT22 {name}")
 
-    for name, bmp280 in bmp280_dict.items():
-        try:
-            temperature = bmp280.temperature
-            print(f"{name} - Temperature: {temperature:.2f} C")
-            sensors_data[name] = {"temperature": temperature}
-        except:
-            print(f"Not found BMP280 {name}")
-            
-    try:
-        if sensors_data:
-            _ = requests.post(endpoint, json=sensors_data)
-    except Exception as e:
-        print(f"Error while making request: {e}")
-
-    time.sleep(2)
+if __name__ == "__main__":
+    main(debug=False)
